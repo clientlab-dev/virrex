83f383dd41270a2de474992711e772473f8a727b
akim1995 <a.khalitov@clientlab.ru>
2018-08-07 15:20:03 +0200

    Changed footer file

--- footer.php
+++ footer.php
@@ -1,3 +1,93 @@
+<footer class="footer">
+    <div class="footer__col footer__col--1">
+        <ul class="footer__list">
+            <li class="footer__list-item footer__top-list-item">
+                <a href="#" class="footer__list-link">БИЗНЕС</a>
+            </li>
+            <li class="footer__list-item">
+                <a href="#" class="footer__list-link">О КОМПАНИИ</a>
+            </li>
+            <li class="footer__list-item">
+                <a href="#" class="footer__list-link">НОВОСТИ</a>
+            </li>
+            <li class="footer__list-item">
+                <a href="#" class="footer__list-link">FAQ</a>
+            </li>
+        </ul>
+    </div>
+    <div class="footer__col footer__col--2">
+        <ul class="footer__list">
+            <li class="footer__list-item footer__top-list-item">
+                Продукты
+            </li>
+            <li class="footer__list-item">
+                Кошелёк
+            </li>
+            <li class="footer__list-item">
+                Вклады
+            </li>
+            <li class="footer__list-item">
+                Торговля
+            </li>
+            <li class="footer__list-item">
+                ICO
+            </li>
+        </ul>
+    </div>
+    <div class="footer__col footer__col--3">
+        <ul class="footer__list">
+            <li class="footer__list-item footer__top-list-item">
+                Мерчант
+            </li>
+            <li class="footer__list-item">
+                API
+            </li>
+            <li class="footer__list-item">
+                VICASH
+            </li>
+        </ul>
+    </div>
+    <div class="footer__col footer__col--4">
+        <ul class="footer__list">
+            <li class="footer__list-item footer__top-list-item">
+                Документы и Презентации
+            </li>
+            <li class="footer__list-item">
+                Пользовательское соглашение
+            </li>
+            <li class="footer__list-item">
+                Партнерское соглашение
+            </li>
+            <li class="footer__list-item">
+                Политика конфиденциальности
+            </li>
+            <li class="footer__list-item">
+                Регистрация компании
+            </li>
+            <li class="footer__list-item">
+                <a href="#" class="footer__list-link footer__list-link--dotted">Бизнес с VIRREX.pdf</a>
+            </li>
+            <li class="footer__list-item">
+                <a href="#" class="footer__list-link footer__list-link--dotted">VIRREX ICO.pdf</a>
+            </li>
+
+        </ul>
+
+    </div>
+    <div class="footer__col footer__col--5">
+        <ul class="footer__list">
+            <li class="footer__list-item footer__top-list-item">
+                В разработке
+            </li>
+            <li class="footer__list-item footer__top-list-item">
+                <a href="#" class="footer__list-item-image"><img src="assets/img/app-store.png" alt="app-store"></a>
+            </li>
+            <li class="footer__list-item footer__top-list-item">
+                <a href="#" class="footer__list-item-image"><img src="assets/img/google-play.png" alt="google-play"></a>
+            </li>
+        </ul>
+    </div>
+</footer>
 </body>
 <link rel="stylesheet" href="css/screen.min.css" />
 <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
