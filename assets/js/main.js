3fbf4be12fcb05d9ce208310ee431f724108d730
clientlab <v.kulyabin@clientlab.ru>
2018-08-09 18:36:03 +0200

    Объеденили JS, выключил автопрефиксер

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,4 +66,62 @@ if ($('.safety').length>0) {
 	});
 };
 
+
+
+	(function() {
+	    var langSwitcher = document.getElementById('langSwitcher');
+	    var langOpen = false;
+	    var arrow = document.getElementsByClassName('footer__dropdown-icon')[0];
+	    langSwitcher.addEventListener('click', function() {
+	            for(var i = 0; i<3; i++) {
+	                langSwitcher.children[i].classList.toggle('footer__lang-switcher--active');
+	            }
+	        if(!langOpen) {
+	            langSwitcher.children[3].style.display = 'block';
+	            langOpen = true;
+	            arrow.classList.toggle('footer__lang-arrow--active');
+	        } else {
+	            langSwitcher.children[3].style.display = 'none';
+	            langOpen = false;
+	            arrow.classList.toggle('footer__lang-arrow--active');
+	        }
+	    })
+	    langSwitcher.addEventListener('mouseenter', function() {
+	        // langSwitcher.classList.toggle('footer__lang-switcher--active');
+	            for(var i = 0; i<3; i++) {
+	                langSwitcher.children[i].classList.toggle('footer__lang-switcher--active');
+	            }
+	    })
+	    langSwitcher.addEventListener('mouseleave', function() {
+	        // langSwitcher.classList.toggle('footer__lang-switcher--active');
+	            for(var i = 0; i<3; i++) {
+	                langSwitcher.children[i].classList.toggle('footer__lang-switcher--active');
+	            }
+	    })
+	}());
+
+	let row = document.querySelector('#row');
+	let size = row.childElementCount;
+	// console.log(row.children);
+	for(let i = 0; i<size; i++) {
+	  var imgSize = row.children[i].firstElementChild.firstElementChild.offsetWidth;
+	  var tooltip = row.children[i].firstElementChild.lastElementChild;
+	  var tooltipSize = row.children[i].firstElementChild.lastElementChild.offsetWidth;
+	  // let difference = tooltipSize - imgSize - imgSize/2;
+	  if(tooltipSize > imgSize) {
+	    var difference = tooltipSize - imgSize;
+	    var result = '-' + difference / 2 + 'px';
+	    tooltip.style.left = result;
+	  } else {
+	    var difference = Math.abs(tooltipSize - imgSize);
+	    var result = difference / 2 + 'px';
+	    tooltip.style.left = result;
+	  }
+	  // console.log(row.children[i]);
+	}
+	// let tooltipSize = document.getElementById('tooltip').offsetWidth;
+	// let imgSize = document.getElementById('img').offsetWidth;
+	// let difference = tooltipSize - imgSize;
+	// let result = difference / 2;
+
 });
