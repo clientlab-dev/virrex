f6661cd9780f5f8632ffd38fa6dcdf5b791df3ba
akim1995 <a.khalitov@clientlab.ru>
2018-08-12 19:42:21 +0200

    new files

---
+++ news-single.php
@@ -0,0 +1,58 @@
+<?php include('includes/header.php'); ?>
+
+<?php 
+$headerOptions = array(
+        'pageColor' => '',
+        'headerTheme' => 'header-dark'
+    );
+
+insert_header($headerOptions);
+?>
+<main class="typography wrap news-single">
+    <div class="news-single__header">
+    <h1>Как вам помочь</h1>
+    </div>
+    <p>
+        The purpose of this HTML is to help determine what default settings are with CSS and
+        to make sure that all possible HTML Elements are included in this HTML so as to not miss any possible
+        Elements when designing a site.
+    </p>
+    <hr>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h3>
+    <h5>Heading 5</h4>
+    <h6>Heading 6</h5>
+    <h2>Paragraph</h2>
+    <p>Lorem ipsum dolor sit amet, <a href="#" title="test link">test link</a> adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
+    <p>Lorem ipsum dolor sit amet, <em>emphasis</em> consectetuer adipiscing <b>Bold</b> elit. Nullam dignissim convallis est. <i>Italic text Italic text Italic text</i> Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
+    <hr>
+    <h3>Ordered List</h3>
+    <ol>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+    </ol>
+    <h3>Unordered List</h3>
+    <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+    </ul>
+    <hr>
+    <p>Lorem ipsum dolor sit, <strong>Strong Text</strong> amet consectetur adipisicing elit. Ea quia consectetur distinctio dolor consequatur hic, dignissimos error nam sed vitae amet animi quas vel id rem veritatis cumque fuga! Blanditiis.
+    Molestias non labore animi dolorum deserunt amet aspernatur <b><i>Bold Italic</i></b> deleniti at ipsam commodi tempora, nostrum sit dignissimos officiis optio rem? Iure velit dicta ut explicabo quam.
+    Repudiandae hic, veritatis minus quis nihil nobis nostrum. Provident voluptatem vel, alias nisi facilis esse quos voluptas ipsa officia voluptate quas accusantium quasi, nobis pariatur. Rem velit minima recusandae quas?
+   <img src="assets/img/news_images/roof.jpg" alt="">
+    Dolorem voluptates, aliquid ipsam dolor reiciendis numquam corporis enim voluptatum mollitia velit placeat doloremque harum nisi quidem magnam architecto tenetur iure voluptatem omnis eaque natus obcaecati accusantium! Accusantium, placeat inventore.</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo error culpa vel quam illum et rem, dolores iusto voluptatem mollitia! Quae odio obcaecati accusamus veniam eos vero tempora nisi repellendus!
+       <br>
+       <br>
+   Ea ratione rem eveniet neque porro nam libero illum? Praesentium quas nesciunt officiis et, ducimus iure porro amet in soluta itaque, minus voluptas? Esse unde totam ullam numquam maxime debitis?
+   Eum obcaecati provident illum vero alias. Odit ducimus beatae ipsam hic illo at animi aut voluptatibus nihil, quia placeat impedit maxime error veritatis suscipit fuga repellendus saepe quidem sint dolore.
+   Vitae culpa vel laboriosam magni quas? Illo porro maiores, hic reiciendis magni perspiciatis eveniet reprehenderit dicta temporibus corporis obcaecati ex incidunt! Dicta quam vel, in tempora quaerat voluptatem dolor ea?
+   Architecto hic est nihil laborum iusto cum accusamus! Dolorem, nam officiis. Impedit maxime ex earum et, unde a, quasi vero natus at expedita, quod atque. Optio asperiores corrupti deleniti ipsa.</p>
+
+</main>
+<?php include('includes/footer.php'); ?>
